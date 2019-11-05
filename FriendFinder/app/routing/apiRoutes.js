d07e6dcf8e199030bfaac27734e473a57b304282
Kshitij Vadake <ktj@Kshitijs-MBP.fios-router.home>
2019-11-05 15:31:00 -0500

    adding routes

---
+++ FriendFinder/app/routing/apiRoutes.js
@@ -0,0 +1,34 @@
+var friends = require("../data/friends");
+
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function (req, res) {
+    var user = req.body;
+    for (var i = 0; i < user.scores.length; i++) {
+      user.scores[i] = parseInt(user.scores[i]);
+    };
+
+    var closestFriendIndex = 0;
+    var minimumDifference = 50;
+
+    for (var i = 0; i < friends.length; i++) {
+      var totalDifference = 0;
+      for (var j = 0; j < friends[i].scores.length; j++) {
+        var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+        totalDifference += difference;
+      }
+
+      if (totalDifference < minimumDifference) {
+        closestFriendIndex = i;
+        minimumDifference = totalDifference;
+      }
+    }
+
+    friends.push(user);
+
+    res.json(friends[closestFriendIndex]);
+  });
+};
