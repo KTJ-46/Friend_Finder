010ace12371be1744fafad6080f785b73cdaef76
Kshitij Vadake <ktj@Kshitijs-MBP.fios-router.home>
2019-11-05 17:25:35 -0500

    added npm packages

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,104 @@
+var friends = [
+    {
+        name: "Rachel Green",
+        photo: "https://vignette.wikia.nocookie.net/friends/images/3/38/RachelGreen.jpg/revision/latest?cb=20180426182043",
+        scores: [
+          "5",
+          "2",
+          "2",
+          "2",
+          "4",
+          "1",
+          "3",
+          "2",
+          "5",
+          "5"
+        ]
+      },
+      {
+        name: "Chandler Bing",
+        photo: "https://vignette.wikia.nocookie.net/friends/images/4/48/Chandler_Bing_portrait.jpg/revision/latest?cb=20100601130820",
+        scores: [
+          "4",
+          "4",
+          "2",
+          "3",
+          "2",
+          "2",
+          "3",
+          "2",
+          "4",
+          "5"
+        ]
+      }
+    {
+      name: "Ross Geller",
+      photo: "https://vignette.wikia.nocookie.net/friends/images/0/0b/RossGeller.jpg/revision/latest?cb=20180424154547",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Phoebe Buffay",
+      photo: "https://vignette.wikia.nocookie.net/friends/images/3/30/PhoebeBuffay.jpg/revision/latest?cb=20180426182547",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    
+    {
+      name: "Monica Geller",
+      photo: "https://vignette.wikia.nocookie.net/friends/images/2/2f/Monica_Geller-Bing_Season_10.png/revision/latest?cb=20180426182234",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Joey Tribiani",
+      photo: "https://vignette.wikia.nocookie.net/friends/images/f/f5/JoeyTribbiani.jpg/revision/latest/scale-to-width-down/699?cb=20180424154245",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    }
+    
+  
+  ];
+  
+  
+  module.exports = friends;
